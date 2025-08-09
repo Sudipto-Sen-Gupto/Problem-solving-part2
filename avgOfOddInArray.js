541dfde9da05b3f626e9781389a0b0e0926da2d2
Sudipta Sen Gupta <gauravsengupto321@gmail.com>
2025-08-09 10:08:19 -0500

    average of odd number from array

---
+++ avgOfOddInArray.js
@@ -0,0 +1,23 @@
+
+function average(list){
+    let avg;
+     let sum=0;
+    let list2=[];
+    for(const item of list){
+           
+        if(item%2!==0){
+            list2.push(item);
+           
+           
+            sum +=item;
+         
+           
+        }
+    }
+     avg=sum/list2.length;
+     console.log(list2);
+      console.log(sum);
+    return avg;
+}
+const arr=average([34,45,33,56,79,105])
+console.log(arr);
