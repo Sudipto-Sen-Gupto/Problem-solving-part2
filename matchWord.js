2a60413bcd6e8f891cf55a204298f68ce0439586
Sudipta Sen Gupta <gauravsengupto321@gmail.com>
2025-08-16 11:54:07 -0500

    matched string checked

---
+++ matchWord.js
@@ -0,0 +1,13 @@
+
+function match(str1,str2){
+   if(typeof str1 !=="string" || typeof str2 !=="string"){
+    return "Invalid";
+   }
+    if(str1.includes(str2)){
+        return true;
+    }
+    else
+        return false;
+}
+const result=match('Peter Parker','pet');
+console.log(result);
